6c6dd3a8d4a8b1489709a82eb711d6736c0ef5de
Jason Koo <jalakoo@gmail.com>
2023-04-18 13:01:49 -0700

    basic rule based ranking added

--- src/app.py
+++ src/app.py
@@ -6,6 +6,8 @@ import openai
 from train_cypher import examples
 from utils import list_from_csv
 from constants import STAR_WARS_SYSTEMS
+from models import Person, System
+import random
 
 # Config
 openai.api_key = st.secrets['OPENAI_KEY']
@@ -21,6 +23,34 @@ def star_wars_systems():
     system_names = [s['Planet'] for s in STAR_WARS_SYSTEMS]
     return system_names
 
+@st.cache_data
+def possible_rebel_systems(minimum_rebel_affinity: float= 0.5):
+    query = f"""
+MATCH (s:System)
+WHERE ANY (affinity IN s.rebel_affinity WHERE affinity > {minimum_rebel_affinity})
+RETURN DISTINCT s
+    """
+    systems = execute_query(query)
+    result = []
+    for r in systems:
+        s = r.get('s')
+        new_system = (System(
+            name=s.get('name', None), 
+            x=s.get('X', None), 
+            y=s.get('Y', None), 
+            region=s.get('Region', None), 
+            type='Rebel System', 
+            importance=s.get('importance', None),
+            rebel_affinity=s.get('rebel_affinity', None)))
+        result.append(new_system)
+    return result
+
+@st.cache_data
+def possible_rebel_system_names(minimum_rebel_affinity: float= 0.5):
+    systems = possible_rebel_systems(minimum_rebel_affinity)
+    names = [s.name for s in systems]
+    return names
+    
 def affinity_as_float(affinity:str)-> float:
     if affinity == "Imperial":
         return 0.0
@@ -48,7 +78,7 @@ MATCH (p)-[:KNOWS]->(c:Character)
 WHERE ANY (name IN t.name WHERE name in $req_skills) AND s2.name = $base
 WITH p, t, s, c, avg(c.rebel_affinity) as avg_affinity
 WHERE avg_affinity >= {reb_affinity}
-RETURN DISTINCT p.name as name, s.name as homeworld, collect(DISTINCT t.name) as skills, collect(DISTINCT c.name) as associates, avg_affinity LIMIT $team_size
+RETURN DISTINCT p.name as name, s.name as homeworld, collect(DISTINCT t.name) as skills, collect(DISTINCT c.name) as associates, avg(c.rebel_affinity) as avg_affinity LIMIT $team_size
     """
     params = {
         'req_skills': req_skills,
@@ -71,6 +101,69 @@ RETURN DISTINCT p.name as name, s.name as homeworld, collect(DISTINCT t.name) as
     return result
 
 
+def devs_with_rank_info(
+    base: str
+):
+    query = f"""
+MATCH (p:Person)-[r:KNOWS]->(t:Topic),
+(p)-[:FROM]->(s:System),
+(p)-[:KNOWS]->(c:Character),
+(base:System),
+path = shortestPath((s)-[:CONNECTED_TO|NEAR*0..100]-(base))
+WHERE base.name = $base
+WITH p, t, c, s, path
+RETURN DISTINCT p.name as name, p.email as email, s.name as homeworld, collect(DISTINCT c.name) as associates, collect(DISTINCT t.name) as devSkills, avg(c.rebel_affinity) as avg_associate_affinity, count(nodes(path)) as jumpsFromBase
+    """
+    params ={
+        'base': base
+    }
+    response = execute_query(query, params)
+    result = []
+    for r in response:
+        print(f'record: {r}')
+        dev = Person(
+            name=r.get('name', None), 
+            email=r.get('email', None),
+            homeworld=r.get('homeworld', None), 
+            skills=r.get('devSkills', None), 
+            associates=r.get('associates', None), 
+            avg_associate_affinity=r.get('avg_associate_affinity', None),
+            jumps_from_base=r.get('jumpsFromBase', None))
+        result.append(dev)
+    return result
+
+def devs_ranked(
+    devs: list[Person],
+    skills: list[str],
+    skills_points_per: float,
+    associate_rebel_affinity: float,
+    associate_rebel_affinity_points_per: float,
+    max_distance_points: float,
+    distance_decay_per_jump: float
+):
+    for dev in devs:
+        dev_score = 0.0
+        skill_points = 0.0
+        affinity_points = 0.0
+        distance_points = 0.0
+        for skill in skills:
+            if skill in dev.skills:
+                skill_points += skills_points_per
+        if dev.avg_associate_affinity is None or dev.avg_associate_affinity < associate_rebel_affinity:
+            affinity_points = 0
+        else:
+            affinity_points = associate_rebel_affinity_points_per * dev.avg_associate_affinity
+        distance_points =  max_distance_points - distance_decay_per_jump * dev.jumps_from_base
+        if distance_points < 0:
+            distance_points = 0
+
+        dev_score += skill_points
+        dev_score += affinity_points
+        dev_score += distance_points
+        dev.ranking_score = dev_score
+    # Return list of devs ranked
+    return sorted(devs, key=lambda x: x.ranking_score, reverse=True)
+
 # UI
 # Convulted way to center image
 col1, col2, col3 = st.columns([1,1,1])
@@ -85,15 +178,8 @@ with col3:
 # st.title("Rebel Developers Network")
 # st.markdown("<h1 style='text-align: center; color: white;'>Rebel Developers Network</h1>", unsafe_allow_html=True)
 
-t1, t2 = st.tabs(["Dashboard", " Manual Search"])
+t1, t2, t3 = st.tabs([" Manual Search", "Ranking", "Data"])
 with t1:
-    # Building mode
-    components.iframe("https://neodash.graphapp.io/", height=1000, scrolling=False)
-
-    # TODO:
-    # View only mode
-
-with t2:
     with st.expander("Advanced Options"):
         team_size = st.slider("Team Size", 1, 12, 6)
         req_skills = st.multiselect("Team programming skills", programming_languages_list(), help="Select the programming languages needed for the team. Can be spread among team members.")
@@ -102,7 +188,7 @@ with t2:
         with t2c1:
             base = st.selectbox("Location of Rebel Development Base", star_wars_systems())
         with t2c2:
-            distance = st.slider("Max Hyperspace Jumps from Base", 1, 25, 5)
+            distance = st.slider("Max Hyperspace Jumps from Base", 1, 100, 10)
 
         reb_affinity = st.select_slider("Political Affinity", ["Imperial", "Imperial Sympathetic", "Neutral", "Rebel Sympathetic", "Rebel"], value = "Neutral")
     
@@ -116,6 +202,57 @@ with t2:
             affinity_as_float(reb_affinity))
         # Display suggested rebel developers
         st.json(developers)
+
+with t2:
+    st.write("TODO")
+    with st.expander("Ranking Rules"):
+        # Base location
+        rebel_bases = possible_rebel_system_names()
+        if len(rebel_bases) == 0:
+            st.write("No rebel bases found")
+            st.stop()
+        else:
+            rebel_base = random.choice(rebel_bases)
+            st.write(f"Rebel Base: {rebel_base}")
+
+        # Desired Skills
+        req_skills = st.multiselect("Team programming skills", programming_languages_list(), help="Select the programming languages needed for the team. Can be spread among team members.", key="ranking_req_skills")
+
+        # Distance Scoring
+        distance_score = st.slider("Max Distance Score", 1, 100, 10)
+        distance_score_dropoff = st.slider("Distance Score Dropoff/jump", 0.0, 10.0, 1.0)
+
+        # Skills Scoring
+        skills_score = st.slider("Points per matching skill", 0, 100, 10)
+
+        # Trustability Scoring
+        trust_score = st.slider("Points per average associate affinity", 0, 100, 10, help="Points per average affinity of associates. Associates are people who know the developer and are also rebel sympathizers. This many points will be assigned for matching the requirement level + this number of points for each .1 above the requirement level.")
+    
+    devs = devs_with_rank_info(rebel_base)
+    devs_ranked = devs_ranked(
+        devs=devs,
+        skills=req_skills,
+        skills_points_per=skills_score,
+        associate_rebel_affinity=affinity_as_float(reb_affinity),
+        associate_rebel_affinity_points_per=trust_score,
+        max_distance_points=distance_score,
+        distance_decay_per_jump=distance_score_dropoff
+    )
+    st.write("Developers Ranked")
+    st.json(devs_ranked)        
+        
+    
+    # Display list of developers by ranking
+
+with t3:
+    # Building mode
+    components.iframe("https://neodash.graphapp.io/", height=1000, scrolling=False)
+
+    # TODO:
+    # View only mode
+
+
+
 
 # Using ChatGPT like search
 # with t2:
--- src/models.py
+++ src/models.py
@@ -7,3 +7,17 @@ class Person:
     skills: list[str]
     associates: list[str]
     homeworld: str
+    matching_skills: int = 0
+    jumps_from_base: int = 0
+    avg_associate_affinity: float = 0.0
+    ranking_score: float = 0.0
+
+@dataclass
+class System:
+    name: str
+    x: float
+    y: float
+    region: str
+    type: str = 'System'
+    importance: float = 0.0
+    rebel_affinity: float = 0.5
